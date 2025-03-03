3e5323d4da6c4ada94ac4c311b558d237328862b
unknown <nurulizzah2727@gmail.com>
2025-03-03 12:36:06 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('hello', function () {
+    return 'hello world';
+});
+
+Route::get('/perkalian/{angka}', function ($angka) {
+    return 'angka ' . $angka*2;
 });
